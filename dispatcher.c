439c7e065c990bdd2f734908bcf7b5fba4db13f4
Fabian Groffen <fabian.groffen@booking.com>
2013-12-07 13:53:10 +0100

    dispatch_connection: allow - (hyphen) in metric names

--- dispatcher.c
+++ dispatcher.c
@@ -173,8 +173,9 @@ dispatch_connection(connection *conn, dispatcher *self)
 			lastnl = NULL;
 			for (p = conn->buf; p - conn->buf < conn->buflen; p++) {
 				if ((*p >= 'a' && *p <= 'z') ||
-						(*p >= 'A' && *p <= 'Z')||
-						(*p >= '0' && *p <= '9'))
+						(*p >= 'A' && *p <= 'Z') ||
+						(*p >= '0' && *p <= '9') ||
+						*p == '-' || *p == '_')
 				{
 					/* copy char */
 					*q++ = *p;
