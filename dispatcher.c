1b8317ef0da0de0e743ab9a42fdd943e85aff5be
Fabian Groffen <fabian.groffen@booking.com>
2013-12-11 20:38:30 +0100

    dispatcher: get size arithmetic right
    We looked at a too small section of the 32K array we allocated for
    storing incoming connections resulting in lockups.

--- dispatcher.c
+++ dispatcher.c
@@ -71,10 +71,10 @@ dispatch_addlistener(int sock)
 	newconn->type = LISTENER;
 	newconn->takenby = 0;
 	newconn->buflen = 0;
-	for (c = 0; c < sizeof(connections); c++)
+	for (c = 0; c < sizeof(connections) / sizeof(connection *); c++)
 		if (__sync_bool_compare_and_swap(&(connections[c]), NULL, newconn))
 			break;
-	if (c == sizeof(connections)) {
+	if (c == sizeof(connections) / sizeof(connection *)) {
 		free(newconn);
 		fprintf(stderr, "cannot add new listener: "
 				"no more free connection slots\n");
@@ -102,10 +102,10 @@ dispatch_addconnection(int sock)
 	newconn->type = CONNECTION;
 	newconn->takenby = 0;
 	newconn->buflen = 0;
-	for (c = 0; c < sizeof(connections); c++)
+	for (c = 0; c < sizeof(connections) / sizeof(connection *); c++)
 		if (__sync_bool_compare_and_swap(&(connections[c]), NULL, newconn))
 			break;
-	if (c == sizeof(connections)) {
+	if (c == sizeof(connections) / sizeof(connection *)) {
 		free(newconn);
 		fprintf(stderr, "cannot add new connection: "
 				"no more free connection slots\n");
@@ -231,10 +231,10 @@ dispatch_connection(connection *conn, dispatcher *self)
 			int c;
 
 			/* find connection */
-			for (c = 0; c < sizeof(connections); c++)
+			for (c = 0; c < sizeof(connections) / sizeof(connection *); c++)
 				if (connections[c] == conn)
 					break;
-			if (c == sizeof(connections)) {
+			if (c == sizeof(connections) / sizeof(connection *)) {
 				/* not found?!? */
 				fprintf(stderr, "PANIC: can't find my own connection!\n");
 				return 1;
@@ -273,7 +273,7 @@ dispatch_runner(void *arg)
 
 	while (keep_running) {
 		work = 0;
-		for (c = 0; c < sizeof(connections) / sizeof(connection); c++) {
+		for (c = 0; c < sizeof(connections) / sizeof(connection *); c++) {
 			conn = connections[c];
 			if (conn == NULL)
 				continue;
