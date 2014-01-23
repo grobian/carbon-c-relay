ac84ef2823fc8eef8abc1b01f89034ac3fe5ae7f
Fabian Groffen <fabian.groffen@booking.com>
2014-01-23 14:36:59 +0100

    relay: parse and reproduce aggregate statements

--- router.c
+++ router.c
@@ -30,7 +30,8 @@
 enum clusttype {
 	FORWARD,
 	CARBON_CH,  /* room for a better/different hash definition */
-	ANYOF
+	ANYOF,
+	AGGREGATION
 };
 
 typedef struct _servers {
@@ -48,6 +49,16 @@ typedef struct _cluster {
 			servers *servers;
 		} carbon_ch;
 		servers *forward;
+		struct {
+			unsigned short interval;  /* when to perform the aggregation */
+			unsigned short expire;    /* when incoming metrics are no longer valid */
+			struct _aggr_computes {
+				enum { SUM, CNT, MAX, MIN, AVG } type;
+				char *metric;         /* name of metric to produce */
+				struct _aggr_computes *next;
+			} *computes;
+			struct _cluster *dest;    /* where produced metrics should go */
+		} aggregation;
 	} members;
 	struct _cluster *next;
 } cluster;
@@ -69,8 +80,23 @@ static cluster *clusters = NULL;
  *
  * Config file supports the following:
  *
- * cluster (name) (forward | round-robin | carbon_ch [replication (count)]) (ip:port[ ip:port ...]) ;
- * match (* | regex) send to (group) [stop] ;
+ * cluster (name)
+ *     (forward | round-robin | carbon_ch [replication (count)])
+ *         (ip:port[ ip:port ...])
+ *     ;
+ * match (* | regex)
+ *     send to (group)
+ *     [stop]
+ *     ;
+ * aggregate
+ *         (regex[ regex ...])
+ *     every (interval) seconds
+ *     expire after (expiration) seconds
+ *     compute (sum | count | max | min | average) write to
+ *         (metric)
+ *     [compute ...]
+ *     send to (group)
+ *     ;
  *
  * Comments start with a #-char.
  *
@@ -341,7 +367,7 @@ router_readconfig(const char *path)
 				free(buf);
 				return 0;
 			}
-			p += 2;
+			p += 3;
 			for (; *p != '\0' && isspace(*p); p++)
 				;
 			dest = p;
@@ -422,6 +448,243 @@ router_readconfig(const char *path)
 			}
 			r->stop = stop;
 			r->next = NULL;
+		} else if (strncmp(p, "aggregate", 9) == 0 && isspace(*(p + 9))) {
+			/* aggregation rule */
+			char *pat;
+			char *dest;
+			char *interval;
+			cluster *w;
+			cluster *s;
+			struct _aggr_computes *ac = NULL;
+
+			p += 10;
+			for (; *p != '\0' && isspace(*p); p++)
+				;
+
+			w = malloc(sizeof(cluster));
+			w->name = NULL;
+			w->type = AGGREGATION;
+			w->next = NULL;
+
+			do {
+				pat = p;
+				for (; *p != '\0' && !isspace(*p); p++)
+					;
+				if (*p == '\0') {
+					fprintf(stderr, "unexpected end of file after 'aggregate'\n");
+					free(buf);
+					return 0;
+				}
+				*p++ = '\0';
+				for (; *p != '\0' && isspace(*p); p++)
+					;
+
+				if (r == NULL) {
+					routes = r = malloc(sizeof(route));
+				} else {
+					r = r->next = malloc(sizeof(route));
+				}
+				if (regcomp(&r->rule, pat, REG_EXTENDED | REG_NOSUB) != 0) {
+					fprintf(stderr, "invalid expression '%s' for aggregation\n",
+							pat);
+					free(r);
+					free(buf);
+					return 0;
+				}
+				r->pattern = strdup(pat);
+				r->dest = w;
+				r->stop = 0;
+				r->matchall = 0;
+				r->next = NULL;
+			} while (strncmp(p, "every", 5) != 0 || !isspace(*(p + 5)));
+			p += 6;
+			for (; *p != '\0' && isspace(*p); p++)
+				;
+			interval = p;
+			for (; *p != '\0' && isdigit(*p); p++)
+				;
+			if (*p == '\0') {
+				fprintf(stderr, "unexpected end of file after 'every %s'\n",
+						interval);
+				free(buf);
+				return 0;
+			}
+			if (!isspace(*p)) {
+				fprintf(stderr, "unexpected character '%c', "
+						"expected number after 'every'\n", *p);
+				free(buf);
+				return 0;
+			}
+			*p++ = '\0';
+			w->members.aggregation.interval = atoi(interval);
+			for (; *p != '\0' && isspace(*p); p++)
+				;
+			if (strncmp(p, "seconds", 7) != 0 || !isspace(*(p + 7))) {
+				fprintf(stderr, "expected 'seconds' after 'every %s'\n",
+						interval);
+				free(buf);
+				return 0;
+			}
+			p += 7;
+			for (; *p != '\0' && isspace(*p); p++)
+				;
+			if (strncmp(p, "expire", 6) != 0 || !isspace(*(p + 6))) {
+				fprintf(stderr, "expected 'expire after' after 'every %s seconds\n",
+						interval);
+				free(buf);
+				return 0;
+			}
+			p += 7;
+			for (; *p != '\0' && isspace(*p); p++)
+				;
+			if (strncmp(p, "after", 5) != 0 || !isspace(*(p + 5))) {
+				fprintf(stderr, "expected 'after' after 'expire'\n");
+				free(buf);
+				return 0;
+			}
+			p += 6;
+			for (; *p != '\0' && isspace(*p); p++)
+				;
+			interval = p;
+			for (; *p != '\0' && isdigit(*p); p++)
+				;
+			if (*p == '\0') {
+				fprintf(stderr, "unexpected end of file after 'expire after %s'\n",
+						interval);
+				free(buf);
+				return 0;
+			}
+			if (!isspace(*p)) {
+				fprintf(stderr, "unexpected character '%c', "
+						"expected number after 'expire after'\n", *p);
+				free(buf);
+				return 0;
+			}
+			*p++ = '\0';
+			w->members.aggregation.expire = atoi(interval);
+			for (; *p != '\0' && isspace(*p); p++)
+				;
+			if (strncmp(p, "seconds", 7) != 0 || !isspace(*(p + 7))) {
+				fprintf(stderr, "expected 'seconds' after 'expire after %s'\n",
+						interval);
+				free(buf);
+				return 0;
+			}
+			p += 8;
+			for (; *p != '\0' && isspace(*p); p++)
+				;
+			do {
+				if (strncmp(p, "compute", 7) != 0 || !isspace(*(p + 7))) {
+					fprintf(stderr, "expected 'compute' at %s'\n", p);
+					free(buf);
+					return 0;
+				}
+				p += 8;
+				pat = p;
+				for (; *p != '\0' && !isspace(*p); p++)
+					;
+				if (*p == '\0') {
+					fprintf(stderr, "unexpected end of file after 'compute'\n");
+					free(buf);
+					return 0;
+				}
+				*p++ = '\0';
+
+				if (ac == NULL) {
+					ac = w->members.aggregation.computes = malloc(sizeof(struct _aggr_computes));
+				} else {
+					ac = ac->next = malloc(sizeof(struct _aggr_computes));
+				}
+				if (strcmp(pat, "sum") == 0) {
+					ac->type = SUM;
+				} else if (strcmp(pat, "count") == 0) {
+					ac->type = CNT;
+				} else if (strcmp(pat, "max") == 0) {
+					ac->type = MAX;
+				} else if (strcmp(pat, "min") == 0) {
+					ac->type = MIN;
+				} else if (strcmp(pat, "average") == 0) {
+					ac->type = AVG;
+				} else {
+					fprintf(stderr, "expected sum, count, max, min or average "
+							"after 'compute', got '%s'\n", pat);
+					free(buf);
+					return 0;
+				}
+				for (; *p != '\0' && isspace(*p); p++)
+					;
+				if (strncmp(p, "write", 5) != 0 || !isspace(*(p + 5))) {
+					fprintf(stderr, "expected 'write to' after 'compute %s'\n", pat);
+					free(buf);
+					return 0;
+				}
+				p += 6;
+				for (; *p != '\0' && isspace(*p); p++)
+					;
+				if (strncmp(p, "to", 2) != 0 || !isspace(*(p + 2))) {
+					fprintf(stderr, "expected 'write to' after 'compute %s'\n", pat);
+					free(buf);
+					return 0;
+				}
+				p += 3;
+				for (; *p != '\0' && isspace(*p); p++)
+					;
+				pat = p;
+				for (; *p != '\0' && !isspace(*p); p++)
+					;
+				if (*p == '\0') {
+					fprintf(stderr, "unexpected end of file after 'write to'\n");
+					free(buf);
+					return 0;
+				}
+				*p++ = '\0';
+				ac->metric = strdup(pat);
+				ac->next = NULL;
+				for (; *p != '\0' && isspace(*p); p++)
+					;
+			} while (strncmp(p, "send", 4) != 0 || !isspace(*(p + 4)));
+			p += 5;
+			for (; *p != '\0' && isspace(*p); p++)
+				;
+			if (strncmp(p, "to", 2) != 0 || !isspace(*(p + 2))) {
+				fprintf(stderr, "expected 'send to' after 'write to %s'\n", pat);
+				free(buf);
+				return 0;
+			}
+			p += 3;
+			for (; *p != '\0' && isspace(*p); p++)
+				;
+			dest = p;
+			for (; *p != '\0' && !isspace(*p) && *p != ';'; p++)
+				;
+			if (*p == '\0') {
+				fprintf(stderr, "unexpected end of file after 'send to %s'\n",
+						dest);
+				free(buf);
+				return 0;
+			}
+			*p++ = '\0';
+			for (; *p != '\0' && isspace(*p); p++)
+				;
+			if (*p == ';') {
+				p++;
+			} else {
+				fprintf(stderr, "expected ';' after '%s' for aggregate\n", dest);
+				free(buf);
+				return 0;
+			}
+
+			/* lookup dest */
+			for (s = clusters; s != NULL; s = s->next) {
+				if (strcmp(s->name, dest) == 0)
+					break;
+			}
+			if (s == NULL) {
+				fprintf(stderr, "no such cluster '%s' for aggregate\n", dest);
+				free(buf);
+				return 0;
+			}
+			w->members.aggregation.dest = s;
 		} else {
 			/* garbage? */
 			fprintf(stderr, "garbage in config: %s\n", p);
@@ -463,10 +726,33 @@ router_printconfig(FILE *f)
 	}
 	fprintf(f, "\n");
 	for (r = routes; r != NULL; r = r->next) {
-		fprintf(f, "match %s\n\tsend to %s%s;\n",
-				r->matchall ? "*" : r->pattern,
-				r->dest->name,
-				r->stop ? "\n\tstop" : "");
+		if (r->dest->type == AGGREGATION) {
+			cluster *aggr = r->dest;
+			struct _aggr_computes *ac;
+
+			fprintf(f, "aggregate\n");
+			do {
+				fprintf(f, "\t\t%s\n", r->pattern);
+			} while (r->next != NULL && r->next->dest == aggr
+					&& (r = r->next) != NULL);
+			fprintf(f, "\tevery %u seconds\n"
+					"\texpire after %u seconds\n",
+					aggr->members.aggregation.interval,
+					aggr->members.aggregation.expire);
+			for (ac = aggr->members.aggregation.computes; ac != NULL; ac = ac->next)
+				fprintf(f, "\tcompute %s write to\n"
+						"\t\t%s\n",
+						ac->type == SUM ? "sum" : ac->type == CNT ? "count" :
+						ac->type == MAX ? "max" : ac->type == MIN ? "min" :
+						ac->type == AVG ? "average" : "<unknown>", ac->metric);
+			fprintf(f, "\tsend to %s\n"
+					"\t;\n", aggr->members.aggregation.dest->name);
+		} else {
+			fprintf(f, "match %s\n\tsend to %s%s\n\t;\n",
+					r->matchall ? "*" : r->pattern,
+					r->dest->name,
+					r->stop ? "\n\tstop" : "");
+		}
 	}
 	fflush(f);
 }
@@ -515,6 +801,10 @@ router_route(const char *metric_path, const char *metric)
 					for (i = 0; i < w->dest->members.carbon_ch.repl_factor; i++)
 						server_send(dests[i], metric);
 				}	break;
+				case AGGREGATION: {
+					/* TODO */
+					break;
+				}
 			}
 
 			/* stop processing further rules if requested */
