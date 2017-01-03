a0427b4276a0cd1c2fd0654cdc9cebf1cdf801f3
Fabian Groffen <grobian@gentoo.org>
2017-01-03 11:37:05 +0100

    update copyright

--- server.h
+++ server.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2016 Fabian Groffen
+ * Copyright 2013-2017 Fabian Groffen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
